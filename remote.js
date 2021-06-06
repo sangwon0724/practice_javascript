63446417521fb66becf0906d6a183fd54b3a850c
sangwon0724 <leews0724@naver.com>
2021-06-07 01:47:43 +0900

    test remote js's update for web

--- remote.js
+++ remote.js
@@ -2,7 +2,9 @@
 function move_section(event){
     var section_id = event.target.value;
     //location.href = "#" + section_id;
-    document.querySelector("#note_body > #"+section_id).scrollIntoView({behavior: 'smooth'});
+    document.getElementById(section_id).scrollIntoView({behavior: 'smooth'});
 }
 
-document.querySelector("#note_body > #remote_menu").addEventListener("change", move_section);
+//document.getElementById("remote_menu").addEventListener("change", move_section);
+
+$("#note_body > #remote> #remote_setcion > #remote_menu").on("change",move_section)
