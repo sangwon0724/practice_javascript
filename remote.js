7261b8ff43f94ea397a90c10d9176cbf7b387aee
sangwon0724 <leews0724@naver.com>
2021-06-07 00:59:39 +0900

    test for error

--- remote.js
+++ remote.js
@@ -1,8 +1,9 @@
 //리모콘 작동
 function move_section(event){
     var section_id = event.target.value;
+    console.log(section_id);
     //location.href = "#" + section_id;
-    document.getElementById(section_id).scrollIntoView({behavior: 'smooth'});
+    document.getElementById("note_body").getElementById(section_id).scrollIntoView({behavior: 'smooth'});
 }
 
-document.getElementById("remote_menu").addEventListener("change", move_section);
+document.getElementById("note_body").getElementById("remote_menu").addEventListener("change", move_section);
